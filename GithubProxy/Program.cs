942eaeb66b5afa362579c6efc5d6efbfefb9ecf9
Brian Barrett <blaxbb@gmail.com>
2022-03-27 00:51:50 -0400

    init

---
+++ GithubProxy/Program.cs
@@ -0,0 +1,58 @@
+using System.Net;
+using System.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+HttpClient client = new HttpClient();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+app.MapPost("/github-proxy", async (context) => {
+    if (!context.Request.HasJsonContentType())
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.UnsupportedMediaType;
+        return;
+    }
+
+    var oauth = await context.Request.ReadFromJsonAsync<OauthRequest>();
+    if (oauth == null)
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        return;
+    }
+
+    var response = await client.PostAsJsonAsync("https://github.com/login/device/code", oauth);
+    if (!response.IsSuccessStatusCode)
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        return;
+    }
+
+    var oauthResp = await response.Content.ReadFromJsonAsync<OauthResponse>();
+    if(oauthResp == null)
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        return;
+    }
+
+    await context.Response.WriteAsJsonAsync(oauthResp);
+});
+
+app.Run();
+
+internal record OauthRequest(string client_id, List<string> scopes)
+{
+
+}
+
+internal record OauthResponse(string device_code, string user_code, string verification_uri, int expires_in, int interval)
+{
+
+}

8c6aa1c7909fa15eabe72920464273bb54971dc2
Brian Barrett <blaxbb@gmail.com>
2022-03-27 04:02:41 -0400

    Added access_token endpoint

--- GithubProxy/Program.cs
+++ GithubProxy/Program.cs
@@ -20,14 +20,14 @@ app.UseHttpsRedirection();
 HttpClient client = new HttpClient();
 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-app.MapPost("/github-proxy", async (context) => {
+app.MapPost("/login/device/code", async (context) => {
     if (!context.Request.HasJsonContentType())
     {
         context.Response.StatusCode = (int)HttpStatusCode.UnsupportedMediaType;
         return;
     }
 
-    var oauth = await context.Request.ReadFromJsonAsync<OauthRequest>();
+    var oauth = await context.Request.ReadFromJsonAsync<OauthFlowRequest>();
     if (oauth == null)
     {
         context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
@@ -41,8 +41,40 @@ app.MapPost("/github-proxy", async (context) => {
         return;
     }
 
-    var oauthResp = await response.Content.ReadFromJsonAsync<OauthResponse>();
+    var oauthResp = await response.Content.ReadFromJsonAsync<OauthFlowResponse>();
     if(oauthResp == null)
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        return;
+    }
+
+    await context.Response.WriteAsJsonAsync(oauthResp);
+});
+
+
+app.MapPost("/login/oauth/access_token", async (context) => {
+    if (!context.Request.HasJsonContentType())
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.UnsupportedMediaType;
+        return;
+    }
+
+    var oauth = await context.Request.ReadFromJsonAsync<OauthTokenRequest>();
+    if (oauth == null)
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        return;
+    }
+
+    var response = await client.PostAsJsonAsync("https://github.com/login/oauth/access_token", oauth);
+    if (!response.IsSuccessStatusCode)
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        return;
+    }
+    var str = await response.Content.ReadAsStringAsync();
+    var oauthResp = await response.Content.ReadFromJsonAsync<OauthTokenResponse>();
+    if (oauthResp == null)
     {
         context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
         return;
@@ -53,12 +85,8 @@ app.MapPost("/github-proxy", async (context) => {
 
 app.Run();
 
-internal record OauthRequest(string client_id, List<string> scopes)
-{
+internal record OauthFlowRequest(string client_id, List<string> scopes);
+internal record OauthFlowResponse(string device_code, string user_code, string verification_uri, int expires_in, int interval);
 
-}
-
-internal record OauthResponse(string device_code, string user_code, string verification_uri, int expires_in, int interval)
-{
-
-}
+internal record OauthTokenRequest(string client_id, string device_code, string user_code, string grant_type);
+internal record OauthTokenResponse(string token_type, string access_token, string scope, string error, string error_description, string error_uri);
